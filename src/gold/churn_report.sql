4eb1923e5fecea117fd443484cb5272bfabfb380
teomewhy@gmail.com <teomewhy@gmail.com>
2024-07-27 20:02:19 +0000

    Churn Report

---
+++ src/gold/churn_report.sql
@@ -0,0 +1,38 @@
+WITH tb_new AS (
+
+  SELECT DISTINCT
+        date('{dt_ref}')AS dtRef,
+        t1.idCliente
+
+  FROM silver.upsell.transacoes AS t1
+
+  WHERE DATE(t1.dtTransacao) <= '{dt_ref}'
+  AND DATE(t1.dtTransacao) > '{dt_ref}' - INTERVAL 28 DAY
+
+),
+
+tb_old AS (
+
+  SELECT DISTINCT
+        date('{dt_ref}' - INTERVAL 28 DAY)AS dtRef,
+        t1.idCliente
+
+  FROM silver.upsell.transacoes AS t1
+
+  WHERE DATE(t1.dtTransacao) <= '{dt_ref}' - INTERVAL 28 DAY
+  AND DATE(t1.dtTransacao) > '{dt_ref}' - INTERVAL 56 DAY
+  
+)
+
+select '{dt_ref}' AS dtRef,
+       count(t1.idCliente) AS qtdeBaseOld,
+       count(t2.idCliente) AS qtdeBaseNewNotChurn,
+       count(t1.idCliente) - count(t2.idCliente)  AS nrQtdeChurn,
+       count(t2.idCliente) / count(t1.idCliente) AS ChurnRate
+
+FROM tb_old as t1
+
+LEFT JOIN tb_new AS t2
+ON t1.idCliente = t2.idCliente
+
+GROUP BY ALL
